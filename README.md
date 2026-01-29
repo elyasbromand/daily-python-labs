193a9387d55fb5f8944d8ff63e94b74eef8a86b8
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-01-29 22:03:01 +0430

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Daily Python Labs Repo
+This repo showcases my daily python scripts which are some good tools and scripts to code in.
---
+++ hello.py
@@ -0,0 +1,4 @@
+# Ask the user for their name and greet them
+name = input("What's your name? ")
+name = name.title()
+print(f"hello, {name}" )
