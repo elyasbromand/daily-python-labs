88f8342b6cf821d389abfa92c471e5c871c07156
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-01-30 23:35:42 +0430

    Code parity

--- compare.py
+++ compare.py
@@ -1,13 +1,9 @@
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
-if x < y:
-    print("x is less than y")
-
-elif x > y:
-    print("x is greater than y")
-
-else x == y:
+if x != y:
+    print("x and y are not equal")
+else:
     print("x is equal to y")
 
 
