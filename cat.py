80c9584a2960a7472ec3479da718392ac1462a5e
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-01-31 21:01:20 +0430

    Apply Dictionaries

--- cat.py
+++ cat.py
@@ -1,13 +1,16 @@
+# Starting point
 def main():
     number = get_number()
     meow(number)
 
+# Get a positive integer
 def get_number():
     while True:
         number = int(input("Enter a number: "))
         if number > 0:
             return number
 
+# print Meow number times
 def meow(times):
     print("Meow\n" * times, end="")
 
---
+++ hogwarts.py
@@ -0,0 +1,9 @@
+students = {
+    "Hermione": "Gryffindor",
+    "Draco": "Slytherin",
+    "Luna": "Ravenclaw",
+    }
+
+for student in students:
+    print(student, students[student], sep = "=> ")
+
