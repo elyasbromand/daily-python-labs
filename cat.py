d670ae7e71cd7176104880600a199ac20e29d716
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-01-31 19:30:14 +0430

    Apply While True repetation

--- cat.py
+++ cat.py
@@ -1,1 +1,15 @@
-print("Meow \n" * 3, end="") 
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        number = int(input("Enter a number: "))
+        if number > 0:
+            return number
+
+def meow(times):
+    print("Meow\n" * times, end="")
+
+
+main()
