01301a24feca317f0d2f9c8c13ce6dba4ae0f16a
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-02-06 20:54:23 +0430

    Experience UnboundLocalError

--- exception.py
+++ exception.py
@@ -1,13 +1,13 @@
-def main(): 
-    while True:
-        x = get_number()
-        if x < 0:
-            print("Negative numbers are not allowed.")
-            break
-        else:
-            print(f"You entered: {x}")
-def get_number():
-    return int(input("Enter a number: "))
+try:
+    x = int(input("What's x? "))
+    
+except ValueError:
+    print ("X is not a valid integer.")
+
+print(f"X is {x}")
 
 
-main()
+# In the code above if the input from the user is something other than integer then we will have the value error in the except 
+# block and the program will print "X is not a valid integer." and then it will try to print the value of x but since x is not 
+# defined in the except block it will raise another error which is UnboundLocalError. To avoid this we can initialize x with a 
+# default value before the try block.
