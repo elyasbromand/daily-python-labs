f1a57a2f2f1d9b486c04cfb797d2274810e548e7
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-02-06 21:07:38 +0430

    Use proper Error Handling and Exceptions

--- exception.py
+++ exception.py
@@ -3,11 +3,21 @@ try:
     
 except ValueError:
     print ("X is not a valid integer.")
-
-print(f"X is {x}")
+else:
+    print(f"X is {x}")
 
 
 # In the code above if the input from the user is something other than integer then we will have the value error in the except 
 # block and the program will print "X is not a valid integer." and then it will try to print the value of x but since x is not 
 # defined in the except block it will raise another error which is UnboundLocalError. To avoid this we can initialize x with a 
 # default value before the try block.
+
+# try:
+       # Some Code.... 
+# except:
+       # optional block
+       # Handling of exception (if required)
+# else:
+       # execute if no exception
+# finally:
+      # Some code .....(always executed)
