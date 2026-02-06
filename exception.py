2f1574838034a0e5e26fa473c385f826ea6e4615
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-02-06 21:43:10 +0430

    Pass the Exception

--- exception.py
+++ exception.py
@@ -1,23 +1,16 @@
-try:
-    x = int(input("What's x? "))
-    
-except ValueError:
-    print ("X is not a valid integer.")
-else:
-    print(f"X is {x}")
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            x = int(input("What's x? "))
+            
+        except ValueError:
+            pass
+        else:
+            return x
 
 
-# In the code above if the input from the user is something other than integer then we will have the value error in the except 
-# block and the program will print "X is not a valid integer." and then it will try to print the value of x but since x is not 
-# defined in the except block it will raise another error which is UnboundLocalError. To avoid this we can initialize x with a 
-# default value before the try block.
-
-# try:
-       # Some Code.... 
-# except:
-       # optional block
-       # Handling of exception (if required)
-# else:
-       # execute if no exception
-# finally:
-      # Some code .....(always executed)
+main()
